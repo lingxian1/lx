f7a5fce6ea47ffd0205c118568f5f68a0c11a07b
lingxian1 <cyk6642@163.com>
2017-10-19 14:46:55 +0800

    1.30 refactoring code

---
+++ src/main/java/com/exam/config/CustomWebMvcConfigurerAdapter.java
@@ -0,0 +1,13 @@
+package com.exam.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class CustomWebMvcConfigurerAdapter extends WebMvcConfigurerAdapter {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new UserInterceptor()).addPathPatterns("/**");  //对来自/user/** 这个链接来的请求进行拦截
+    }
+}
---
+++ src/main/java/com/exam/config/UserInterceptor.java
@@ -0,0 +1,32 @@
+package com.exam.config;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class UserInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        Cookie[] cookies = request.getCookies();
+        if (cookies!=null && cookies.length>0) {
+            for (Cookie cookie : cookies) {
+                System.out.println(cookie.getName()+":"+cookie.getValue());
+
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
+}
